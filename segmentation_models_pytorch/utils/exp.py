77ce86f31c93ac79f4b2b13be207e1050785c48a
arvind <arvind.m.vepa@gmail.com>
2021-08-12 21:27:01 -0700

    debug

--- segmentation_models_pytorch/utils/exp.py
+++ segmentation_models_pytorch/utils/exp.py
@@ -300,9 +300,8 @@ def train_net(data_dir='/home/ubuntu/work/vessel_seg/data/DRIVE/train/images',
 
         if reduce_lr_on_plateau is not None:
             if cur_epoch % val_freq == 0:
-                val_metrics = {valid_metric: valid_logs[valid_metric]
-                               for valid_metric in valid_logs.keys() if metric in valid_metric}
-                lr_scheduler.step(val_metrics[reduce_lr_on_plateau.get("metric", 'auc_roc')], epoch=epoch)
+                print(valid_logs.keys())
+                lr_scheduler.step(valid_logs[reduce_lr_on_plateau.get("metric", 'auc_roc')], epoch=epoch)
         else:
             for lr, epoch in lr_schedule:
                 if i == epoch:
